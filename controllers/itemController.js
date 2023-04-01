c5f5057b9b7b5d7a30c4eb8c11e196ff8ae7c787
naknamu <jodeldelvalle@gmail.com>
2023-04-01 21:07:46 +0800

    Added view for create item form

--- controllers/itemController.js
+++ controllers/itemController.js
@@ -1,6 +1,9 @@
 const Item = require("../models/item");
 const Category = require("../models/category");
 
+const async = require("async");
+const { body, validationResult } = require("express-validator");
+
 // Display list of all items
 exports.item_list = (req, res) => {
     // res.send("NOT IMPLEMENTED: Item list");
@@ -40,13 +43,75 @@ exports.item_detail = (req, res, next) => {
 
 // Display item CREATE form on GET
 exports.item_create_get = (req, res) => {
-    res.send("NOT IMPLEMENTED: Item create GET");
+    // res.send("NOT IMPLEMENTED: Item create GET");
+    // Get all categories, which will be used for adding item
+    Category.find()
+      .sort({name: 1})
+      .exec(function(err, list_categories) {
+        if (err) {
+            return next(err);
+        }
+        // Successful, so render
+        res.render("item_form", { 
+            title: "Create Item",
+            category_list: list_categories
+        });
+    })
 }
 
 // Handle item CREATE on POST
-exports.item_create_post = (req, res) => {
-    res.send("NOT IMPLEMENTED: Item create POST");
-}
+exports.item_create_post = [
+    // Validate and sanitize the name field.
+    body("name", "Item name must not be empty.").trim().isLength({ min: 1 }).escape(),
+    body("description", "Item description must not be empty.").trim().isLength({ min: 1 }).escape(),
+    body("category").escape(),
+    body("price", "Item price must not be empty.").trim().isLength({ min: 1 }).escape(),
+    body("number_in_stock", "Item number in stock must not be empty.").trim().isLength({ min: 1 }).escape(),
+
+    // Process request after validation and sanitization
+    (req, res, next) => {
+        // Extract the validation errors from a request
+        const errors = validationResult(req);
+
+        //Create a category object with escaped and trimmed data
+        const item = new Item({
+            name: req.body.name, 
+            description: req.body.description,
+            category: req.body.category,
+            price: req.body.price,
+            number_in_stock: req.body.number_in_stock,
+        });
+
+        // res.send(req.body.category);
+
+        if (!errors.isEmpty()) {
+            // There are errors. Render the form again with sanitized value/error messages
+            Category.find()
+              .sort({name: 1})
+              .exec(function(err, list_categories) {
+            if (err) {
+                return next(err);
+            }
+            // rerender
+            res.render("item_form", { 
+                title: "Create Item",
+                category_list: list_categories,
+                item,
+                errors: errors.array(),
+                });
+            })
+            return;
+        } 
+        // Data from form is valid. Save item
+        item.save((err) => {
+            if (err) {
+                return next(err);
+            }
+            // item saved. Redirect to item detail page
+            res.redirect(item.url);
+        })
+    }
+];
 
 // Display item DELETE form on GET
 exports.item_delete_get = (req, res) => {
